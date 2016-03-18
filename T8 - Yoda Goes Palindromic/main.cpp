9863a93d7e360cd56186dab9b51048923cb58825
Melissa Treviño <mely.trevic@gmail.com>
2016-03-17 23:10:08 -0600

    Initial Commit

---
+++ T8 - Yoda Goes Palindromic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  T8 - Yoda Goes Palindromic
+//
+//  Created by Melissa Trevino on 3/17/16.
+//  Copyright © 2016 Melissa Trevino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
